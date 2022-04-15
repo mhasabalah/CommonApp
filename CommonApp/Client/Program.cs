0f6aca94df1f24fc1cf94fe0fcff2d4dda012c39
Mohamed Hasabalah <68168970+mhasabalah@users.noreply.github.com>
2022-04-15 22:17:50 +0200

    mainlayout

--- CommonApp/Client/Program.cs
+++ CommonApp/Client/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 
+builder.Services.AddScoped<IValidator<EmployeeViewModel>, EmployeeValidator>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
